012b0d138b591437661fc60e16c063fe746782be
maik <moewes@users.noreply.github.com>
2020-12-17 11:10:08 +0100

    div

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import '@vaadin/vaadin-text-field';
 import '@vaadin/vaadin-button';
+
+// Layouts
+import '@vaadin/vaadin-ordered-layout';
+import '@vaadin/vaadin-form-layout';
+import '@vaadin/vaadin-app-layout';
